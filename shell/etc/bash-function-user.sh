01b8d51dbe7f2f2dad7d17234eb5c9cb1d0cc082
AeonDigital <rianna.aeon@gmail.com>
2021-08-23 20:06:03 -0300

    Atualização 2021-08-23

--- shell/etc/bash-function-user.sh
+++ shell/etc/bash-function-user.sh
@@ -2,3 +2,18 @@
 
 # Use este script para adicionar funções que deseja disponibilizar para todas as 
 # seções dos usuários desta máquina.
+
+
+# Use para ativar/desativar o UTF8 do seu shell
+# Para ativar use: ``terminalUTF8 on``
+# Para ativar use: ``terminalUTF8 off``
+terminalUTF8 {
+  if test ".$1" = ".off" ; then    
+    printf '\033%%@'
+    echo -e "UTF-8 off"
+  else
+    printf '\033%%G'
+    echo -e "UTF-8 on"
+    echo -e "\u2705"
+  fi
+}
