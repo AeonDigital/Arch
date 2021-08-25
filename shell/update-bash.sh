2421e3c5ee06b6175d495657b734d9b57232f3ed
AeonDigital <rianna.aeon@gmail.com>
2021-08-25 19:16:25 -0300

    Atualização 2021-08-25

--- shell/update-bash.sh
+++ shell/update-bash.sh
@@ -16,15 +16,15 @@ curl -O https://raw.githubusercontent.com/AeonDigital/Tutorial-Arch/master/shell
 
 
 # Move os arquivos baixados para os locais corretos
-mv -a issue /etc/issue
+mv issue /etc/issue
 
-mv -a bash-aliases.sh /etc/bash-aliases.sh
-mv -a bash-function-pallete256.sh /etc/bash-function-pallete256.sh
-mv -a bash-function-user.sh /etc/bash-function-user.sh
-mv -a bash-message-wellcome-01 /etc/bash-message-wellcome-01
-mv -a bash-message-wellcome-02 /etc/bash-message-wellcome-02
-mv -a bash-prompt.sh /etc/bash-prompt.sh
-mv -a bash-variables.sh /etc/bash-variables.sh
+mv bash-aliases.sh /etc/bash-aliases.sh
+mv bash-function-pallete256.sh /etc/bash-function-pallete256.sh
+mv bash-function-user.sh /etc/bash-function-user.sh
+mv bash-message-wellcome-01 /etc/bash-message-wellcome-01
+mv bash-message-wellcome-02 /etc/bash-message-wellcome-02
+mv bash-prompt.sh /etc/bash-prompt.sh
+mv bash-variables.sh /etc/bash-variables.sh
 
-mv -a .bash_profile /etc/skel/.bash_profile
-mv -a .bashrc /etc/skel/.bashrc
+mv .bash_profile /etc/skel/.bash_profile
+mv .bashrc /etc/skel/.bashrc
