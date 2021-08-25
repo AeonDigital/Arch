cab28779248b9a013c7c279b9fb0fde45e145341
AeonDigital <rianna.aeon@gmail.com>
2021-08-25 00:34:47 -0300

    2021-08-24

--- shell/etc/bash-prompt.sh
+++ shell/etc/bash-prompt.sh
@@ -10,35 +10,35 @@ PSTYLE01="\[\e[40;00;37m\]\$ \u@\h : "
 
 
 ## $ rianna@archlinux : _
-PRINTSTYLE02A="${NONE}${GREEN}$ username@host : ${NONE}"
+PRINTSTYLE02A="${NONE}${GREEN}\$ username@host : ${NONE}"
 PSTYLE02A="\[\e[40;00;32m\]\$ \u@\h : \[\e[40;00;37m\]"
 
 
 ## $ rianna@archlinux : _
-PRINTSTYLE02B="${NONE}${CYAN}$ ${GREEN}username@host ${CYAN}: ${NONE}"
+PRINTSTYLE02B="${NONE}${CYAN}\$ ${GREEN}username@host ${CYAN}: ${NONE}"
 PSTYLE02B="\[\e[40;00;36m\]\$ \[\e[40;00;32m\]\u@\h \[\e[40;00;36m\]: \[\e[40;00;37m\]"
 
 
 ## $ rianna@archlinux : _
-PRINTSTYLE02C="${NONE}${CYAN}$ ${GREEN}username${WHITE}@${GREEN}host ${CYAN}: ${NONE}"
+PRINTSTYLE02C="${NONE}${CYAN}\$ ${GREEN}username${WHITE}@${GREEN}host ${CYAN}: ${NONE}"
 PSTYLE02C="\[\e[40;00;36m\]\$ \[\e[40;00;32m\]\u\[\e[40;00;37m\]@\[\e[40;00;32m\]\h\[\e[40;00;36m\] : \[\e[40;00;37m\]"
 
 
 # $ rianna@archlinux in ~/DirName/SubDir
 # > _
-PRINTSTYLE03A="${NONE}${LBLUE}$ ${GREEN}username${WHITE}@${GREEN}host ${LBLUE}in ${CYAN}~/atual/directory/path \n${CYAN}> ${NONE}"
+PRINTSTYLE03A="${NONE}${LBLUE}\$ ${GREEN}username${WHITE}@${GREEN}host ${LBLUE}in ${CYAN}~/atual/directory/path \n${CYAN}> ${NONE}"
 PSTYLE03A="\[\e[40;01;34m\]\$ \[\e[40;00;32m\]\u\[\e[40;00;37m\]@\[\e[40;00;32m\]\h\[\e[40;01;34m\] in \[\e[40;00;36m\]\w \n\076 \[\e[40;00;37m\]"
 
 
 # ┌── $ rianna@archlinux in ~/DirName/SubDir
 # └─╼ _
-PRINTSTYLE03B="${NONE}${CYAN}\342\224\214\342\224\200\342\224\200${LBLUE} $ ${GREEN}username${WHITE}@${GREEN}host ${LBLUE}in ${CYAN}~/atual/directory/path \n${CYAN}\342\224\224\342\224\200\342\225\274 ${NONE}"
+PRINTSTYLE03B="${NONE}${CYAN}\342\224\214\342\224\200\342\224\200${LBLUE} \$ ${GREEN}username${WHITE}@${GREEN}host ${LBLUE}in ${CYAN}~/atual/directory/path \n${CYAN}\342\224\224\342\224\200\342\225\274 ${NONE}"
 PSTYLE03B="\[\e[40;00;36m\]\342\224\214\342\224\200\342\224\200\[\e[40;01;34m\] \$ \[\e[40;00;32m\]\u\[\e[40;00;37m\]@\[\e[40;00;32m\]\h\[\e[40;01;34m\] in \[\e[40;00;36m\]\w \n\[\e[40;00;36m\]\342\224\224\342\225\274 \[\e[40;00;37m\]"
 
 
 # ┌── $ rianna@archlinux in ~/DirName/SubDir
 # └─> _
-PRINTSTYLE03C="${NONE}${CYAN}\342\224\214\342\224\200\342\224\200${LBLUE} $ ${GREEN}username${WHITE}@${GREEN}host ${LBLUE}in ${CYAN}~/atual/directory/path \n${CYAN}\342\224\224\342\224\200\076 ${NONE}"
+PRINTSTYLE03C="${NONE}${CYAN}\342\224\214\342\224\200\342\224\200${LBLUE} \$ ${GREEN}username${WHITE}@${GREEN}host ${LBLUE}in ${CYAN}~/atual/directory/path \n${CYAN}\342\224\224\342\224\200\076 ${NONE}"
 PSTYLE03C="\[\e[40;00;36m\]\342\224\214\342\224\200\342\224\200\[\e[40;01;34m\] \$ \[\e[40;00;32m\]\u\[\e[40;00;37m\]@\[\e[40;00;32m\]\h\[\e[40;01;34m\] in \[\e[40;00;36m\]\w \n\[\e[40;00;36m\]\342\224\224\342\224\200\076 \[\e[40;00;37m\]"
 
 
--- shell/etc/bash-variables.sh
+++ shell/etc/bash-variables.sh
@@ -16,28 +16,28 @@ HISTTIMEFORMAT="%d/%m/%y %T "
 
 NONE="\e[00m"
 
-BLACK="\e[30m"
-DGREY="\e[1;30m"
-WHITE="\e[37m"
-SILVER="\e[1;37m"
+BLACK="\e[00;30m"
+DGREY="\e[01;30m"
+WHITE="\e[00;37m"
+SILVER="\e[01;37m"
 
-RED="\e[31m"
-LRED="\e[1;31m"
+RED="\e[00;31m"
+LRED="\e[01;31m"
 
-GREEN="\e[32m"
-LGREEN="\e[1;32m"
+GREEN="\e[00;32m"
+LGREEN="\e[01;32m"
 
-BROWN="\e[33m"
-YELLOW="\e[1;33m"
+BROWN="\e[00;33m"
+YELLOW="\e[01;33m"
 
-BLUE="\e[34m"
-LBLUE="\e[1;34m"
+BLUE="\e[00;34m"
+LBLUE="\e[01;34m"
 
-PURPLE="\e[35m"
-LPURPLE="\e[1;35m"
+PURPLE="\e[00;35m"
+LPURPLE="\e[01;35m"
 
-CYAN="\e[36m"
-LCYAN="\e[1;36m"
+CYAN="\e[00;36m"
+LCYAN="\e[01;36m"
 
 
 
