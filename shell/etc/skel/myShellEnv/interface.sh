3f7a3967b3783c312c70c3c92d7e7871e1e8af8b
AeonDigital <rianna.aeon@gmail.com>
2021-08-28 12:04:15 -0300

    Atualizações 2021-08-28

--- shell/etc/skel/myShellEnv/interface.sh
+++ shell/etc/skel/myShellEnv/interface.sh
@@ -277,22 +277,38 @@ reinstallMyShellEnv() {
 # que podem ser utilizadas para a estilização das mensagens
 # de interface.
 #
+# Se quiser/precisar, use o 'grep' para filtrar as linhas que deseja.
+#
+#   example
+#     showMyShellEnvColors | grep -in 'azul'
+#     # o parametro '-in' torna a consulta case-insensitive
+#
 showMyShellEnvColors() {
 
   COLOR_TEXT=$(printf "
-    Preto:          BLACK:  ${BLACK}myShellEnv${NONE}   : Marrom:         BROWN:    ${BROWN}myShellEnv${NONE} \n
-    Cinza escuro:   DGREY:  ${DGREY}myShellEnv${NONE}   : Amarelo:        YELLOW:   ${YELLOW}myShellEnv${NONE} \n
-    Branco:         WHITE:  ${WHITE}myShellEnv${NONE}   : Azul:           BLUE:     ${BLUE}myShellEnv${NONE} \n
-    Prata:          SILVER: ${SILVER}myShellEnv${NONE}  : Azul claro:     LBLUE:    ${LBLUE}myShellEnv${NONE} \n
-    Vermelho:       RED:    ${RED}myShellEnv${NONE}     : Purpura:        PURPLE:   ${PURPLE}myShellEnv${NONE} \n
-    Vermelho claro: LRED:   ${LRED}myShellEnv${NONE}    : Purpura claro:  LPURPLE:  ${LPURPLE}myShellEnv${NONE} \n
-    Verde:          GREEN:  ${GREEN}myShellEnv${NONE}   : Ciano:          CYAN:     ${CYAN}myShellEnv${NONE} \n
-    Verde claro:    LGREEN: ${LGREEN}myShellEnv${NONE}  : Ciano claro:    LCYAN:    ${LCYAN}myShellEnv${NONE} \n
+    Normal:         NONE:     \\\\e[00m:    ${NONE}myShellEnv \n
+    Preto:          BLACK:    \\\\e[00;30m: ${BLACK}myShellEnv${NONE} \n
+    Cinza escuro:   DGREY:    \\\\e[01;30m: ${DGREY}myShellEnv${NONE} \n
+    Branco:         WHITE:    \\\\e[00;37m: ${WHITE}myShellEnv${NONE} \n
+    Prata:          SILVER:   \\\\e[01;37m: ${SILVER}myShellEnv${NONE} \n
+    Vermelho:       RED:      \\\\e[00;31m: ${RED}myShellEnv${NONE} \n
+    Vermelho claro: LRED:     \\\\e[01;31m: ${LRED}myShellEnv${NONE} \n
+    Verde:          GREEN:    \\\\e[00;32m: ${GREEN}myShellEnv${NONE} \n
+    Verde claro:    LGREEN:   \\\\e[01;32m: ${LGREEN}myShellEnv${NONE} \n
+
+    Marrom:         BROWN:    \\\\e[00;33m: ${BROWN}myShellEnv${NONE} \n
+    Amarelo:        YELLOW:   \\\\e[01;33m: ${YELLOW}myShellEnv${NONE} \n
+    Azul:           BLUE:     \\\\e[00;34m: ${BLUE}myShellEnv${NONE} \n
+    Azul claro:     LBLUE:    \\\\e[01;34m: ${LBLUE}myShellEnv${NONE} \n
+    Purpura:        PURPLE:   \\\\e[00;35m: ${PURPLE}myShellEnv${NONE} \n
+    Purpura claro:  LPURPLE:  \\\\e[01;35m: ${LPURPLE}myShellEnv${NONE} \n
+    Ciano:          CYAN:     \\\\e[00;36m: ${CYAN}myShellEnv${NONE} \n
+    Ciano claro:    LCYAN:    \\\\e[01;36m: ${LCYAN}myShellEnv${NONE} \n
   ")
 
   printf "\nAs seguintes opções de cores estão disponíveis: \n\n"
   COLOR_TEXT=$(sed 's/^\s*//g' <<< "${COLOR_TEXT}" | sed 's/\s*$//g' | sed 's/\s*:/:/g' | sed 's/:\s*/:/g')
-  column -s ":" -o " | " -t -N "Cor,Codigo,Aparencia,Cor,Codigo,Aparencia" <<< "${COLOR_TEXT}"
+  column -s ":" -o " | " -t -N "Cor,Raw,Variavel,Aparencia" <<< "${COLOR_TEXT}"
   printf "\n"
 }
 #
